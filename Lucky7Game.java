e99507e0b8b0ee2c0e6a882b783e9536d13c6e75
Ranithudinsara <ranithudinsara@gmail.com>
2025-09-15 15:16:16 +0530

    Printing Game Title and Asking user how much money the user will you to play the game, and display it

--- Lucky7Game.java
+++ Lucky7Game.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 public class Lucky7Game{
     public static void main(String[] args){
 
-        
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
+
+        int luckyNumber = 7;
+
+        //Game title
+        System.out.println("            *****************   ");
+        System.out.println("            Welcome to Lucky7   ");
+        System.out.println("            *****************   ");
+
+        // Ask how much money the user will use
+        System.out.print("\nHow much money do you want to start with? ");
+        int money = in.nextInt();
+        in.nextLine();
+
+        System.out.println("Game starting with " + money + "€\n");
     }
 }
