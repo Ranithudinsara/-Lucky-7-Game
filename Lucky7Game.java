9740985d7a9606275605d1a09b0c928fc3c0ed52
Ranithudinsara <ranithudinsara@gmail.com>
2025-09-15 15:53:28 +0530

    Check how many 7s the player got. Add winnings based on the count of 7s using if else if. End the game if money reaches 0. Ask the player if they want to play another round

--- Lucky7Game.java
+++ Lucky7Game.java
@@ -36,6 +36,38 @@ public class Lucky7Game{
                 }
             }
 
+            //Counting the winnings
+            if (sevens == 3) {
+                money += 10;
+                System.out.println("Three 7s! Jackpot! You win 10€");
+            } else if (sevens == 2) {
+                money += 5;
+                System.out.println("Two 7s! You win 5€");
+            } else if (sevens == 1) {
+                money += 3;
+                System.out.println("One 7! You win 3€");
+            } else {
+                System.out.println("You lost");
+            }
+
+            //if users money is 0 then the game ends
+            if (money <= 0) {
+                System.out.println("Thanks for playing");
+                break;
+            }
+
+            System.out.println("You now have " + money + "€");
+            System.out.println("Do you want to play again? Y/N");
+            String again = in.nextLine();
+
+            if (!again.equalsIgnoreCase("Y")) {
+                System.out.println("Thanks for playing");
+                break;
+            }
+        }
+
+        in.close();
+    }
 
         }
     }
